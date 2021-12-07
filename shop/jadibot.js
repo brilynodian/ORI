854da6a78ec546048e6948637147e359451c407c
brilynodian <93477249+brilynodian@users.noreply.github.com>
2021-12-07 14:04:44 +0700

    Add files via upload

---
+++ shop/jadibot.js
@@ -0,0 +1,26 @@
+const jadibut = () =>{
+return`┏━━━━⬣ *LIST JADIBOT*
+┃⬡ _20K / BULAN_
+┃⬡ _50K PERMANEN_
+┗⬣
+•termasuk custom namabot,thumbnailbot,ownerbot,harga sewa dan payment
+SISTEM
+•TF
+•BERI TAU NAMABOT YG AKAN DIBUAT DLL
+•TUNGGU BEBERAPA MENIT
+•SCAN QR YG OWNER BERIKAN
+•BOT LANGSUNG TERSAMBUNG
+├─ 「 👨‍💻  *RUN ON* 」
+│
+│ ⤷ *Heroku*
+│ ⤷ *Railway* 
+│
+├─ 「 📝  *NOTES* 」
+│
+│ ⤷ _Tidak perlu termux dan hemat kuota_
+│ ⤷ Minat? Hubungi https://wa.me/6289512497753
+│
+╰──「 *Thank's* 」
+`
+}
+exports.jadibut = jadibut
---
+++ shop/sewa.js
@@ -0,0 +1,44 @@
+const pc_sewa = (_minggu, __minggu, ___minggu, _bulan, _permanen, __permanen) => {
+return `*BOT TIDAK ON 24JAM*
+
+┏━━━━⬣ *KEUNTUNGAN*
+┃⬡ _BISA PAKE FITUR PREMIUM_
+┃⬡ _BISA MASUKIN BOT KE GC_
+┃⬡ _TIDAK ADA LIMIT_
+┗⬣
+┏━━━━⬣ *FITUR BOT*
+┃⬡ _ANTILINK_
+┃⬡ _WELCOME IMAGE + BUTTON_
+┃⬡ _BUTTON CMD_
+┃⬡ _STICKER CMD_
+┃⬡ _DAN BANYAK FITUR LAINNYA_
+┗⬣
+┏━━━━⬣ *LIST SEWA+PREM*
+┃⬡ _SEWA+PREM 5k / BULAN_
+┃⬡ _SEWA+PREM 10k /PERMANEN_
+┗⬣
+┏━━━━⬣ *SISTEM*
+┃⬡ _BISA DI TES DAHULU_
+┃⬡ _SEND LINK GRUP-AKTIFIN-TF-DONE_
+┗⬣ 
+┏━━━━⬣ *LIST JADIBOT*
+┃⬡ _20K / BULAN_
+┃⬡ _50K PERMANEN_
+┗⬣
+•termasuk custom namabot,thumbnailbot,ownerbot,harga sewa dan payment
+SISTEM
+•TF
+•BERI TAU NAMABOT YG AKAN DIBUAT DLL
+•TUNGGU BEBERAPA MENIT
+•SCAN QR YG OWNER BERIKAN
+•BOT LANGSUNG TERSAMBUNG
+┏━━━━⬣ *PAYMENT*
+┃⬡ _PULSA_
+┃⬡ _DANA_
+┗⬣
+┏━━━━⬣ *MINAT HUBUNGI*
+┃⬡ _Wa Owner :wa.me/6289512497753_
+┗⬣ `
+	}
+
+exports.pc_sewa = pc_sewa
